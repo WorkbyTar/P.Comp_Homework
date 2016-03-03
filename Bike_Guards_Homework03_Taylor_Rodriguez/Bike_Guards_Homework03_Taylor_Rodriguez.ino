389e1a441aac29cabcbfe96add7e1dae3c3d9caf
WorkbyTar <rodrt215@newschool.edu>
2016-03-03 15:11:20 -0500

    Update Bike_Guards_Homework03_Taylor_Rodriguez.ino

--- Bike_Guards_Homework03_Taylor_Rodriguez/Bike_Guards_Homework03_Taylor_Rodriguez.ino
+++ Bike_Guards_Homework03_Taylor_Rodriguez/Bike_Guards_Homework03_Taylor_Rodriguez.ino
@@ -1,31 +1,4 @@
-/*
-  Button
 
- Turns on and off a light emitting diode(LED) connected to digital
- pin 13, when pressing a pushbutton attached to pin 2.
-
-
- The circuit:
- * LED attached from pin 13 to ground
- * pushbutton attached to pin 2 from +5V
- * 10K resistor attached to pin 2 from ground
-
- * Note: on most Arduinos there is already an LED on the board
- attached to pin 13.
-
-
- created 2005
- by DojoDave <http://www.0j0.org>
- modified 30 Aug 2011
- by Tom Igoe
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/Button
- */
-
-// constants won't change. They're used here to
-// set pin numbers:
 const int ledButtonPin1 = 2;  //right
 const int ledButtonPin2 = 5; //head
 const int ledButtonPin3 = 4; //left
@@ -33,11 +6,11 @@ const int buzzerButtonPin = 3; //buzz
 //// the number of the pushbutton pin
 const int ledPin1 =  10;  
 const int ledPin2 =  13;
-const int ledPin3 =  12; // the number of the LED pin
+const int ledPin3 =  12; 
 
 const int buzzerPin = 11;
 
-// variables will change:
+
 int ledState1 = LOW;
 int ledState2 = LOW;
 int ledState3 = LOW; 
@@ -62,13 +35,12 @@ long lastDebounceTime3 = 0;
 long debounceDelay3 = 50;
 
 void setup() {
-  // initialize the LED pin as an output:
+
   pinMode(ledPin1, OUTPUT);
   pinMode(ledPin2, OUTPUT);
   pinMode(ledPin3, OUTPUT);
   pinMode(buzzerPin, OUTPUT);
-//  
-  // initialize the pushbutton pin as an input:
+
 
   pinMode(ledButtonPin1, INPUT);
   pinMode(ledButtonPin2, INPUT);
@@ -81,7 +53,6 @@ void setup() {
 }
 
 void loop() {
-  // read the state of the pushbutton value:
 
   int reading1 = digitalRead(ledButtonPin1);
   int reading2 = digitalRead(ledButtonPin2);
@@ -100,7 +71,7 @@ digitalWrite(buzzerPin, LOW);
 //1
  
   if (reading1 != lastButtonState1) {
-    // turn LED on:
+
     lastDebounceTime1 = millis();
   } 
 
@@ -117,10 +88,11 @@ digitalWrite(buzzerPin, LOW);
   
   digitalWrite(ledPin1, ledState1);
   lastButtonState1 = reading1; 
+  
 //2
   
  if (reading2 != lastButtonState2) {
-    // turn LED on:
+
     lastDebounceTime2 = millis();
   } 
 
@@ -142,7 +114,7 @@ digitalWrite(buzzerPin, LOW);
 //3
         
   if (reading3 != lastButtonState3) {
-    // turn LED on:
+
     lastDebounceTime3 = millis();
   } 
 
